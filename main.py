9d00338fd2eaa23eae88ff519dec78bcdbfa5bc3
QuentinSav <61971430+QuentinSav@users.noreply.github.com>
2023-06-13 13:13:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-import wire
 import algorithms
 import cv2
 
-
-img = cv2.imread('img/taj.jpeg', cv2.IMREAD_GRAYSCALE)
-#img = cv2.imread('taj.jpeg')
-img = cv2.flip(img, 0)
-optimizer = algorithms.Optimizer(img)
-line_approx = optimizer.run()
-line_approx.show_end_result()
-
-
-
-# drawing = algorithms.LineApproxBruteForce(img)
-# drawing.plot_summary()
-# drawing.plot_costFunc3d()
-# drawing.plot_distFromLine()
-
-
-
-# drawing.imshow_android()
+img = cv2.imread('./img/yack.jpeg', cv2.IMREAD_GRAYSCALE)
+drawing = algorithms.LineApproxBruteForce(img)
+drawing.optimize()
