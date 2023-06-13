b4c9937dbf1c2b83f92c271ba25f72e0dfab5083
QuentinSav <quentin.savary@bluewin.ch>
2023-06-13 13:14:22 +0200

    Merge preparation

--- algorithms.py
+++ algorithms.py
@@ -5,7 +5,9 @@ import cv2
 class LineApprox:
     """Parent class of the different algorithm"""
 
-    def __init__(self, target_img, n_lines=2000):
+    def __init__(self, target_img, color, n_lines=2000):
+
+        self.color = color
 
         # Hyperparameters
         self.hyper_c = 0.1
@@ -25,8 +27,6 @@ class LineApprox:
 
         # Initialization of reconstruction img
         self.recon_img = np.zeros([self.height, self.width])
-
-        print('Target image size: width=', self.width, ', height=', self.height, )
 
     def normalize(self, img):
         """Function that returns the normalized image"""
@@ -77,10 +77,18 @@ class LineApprox:
         ax_array[0].set_xticks([])
         ax_array[0].set_yticks([])
 
+        fig.show()
+
         # Draw all the computed lines
         ax_array[1].set_title("Line approximation image")
+
         for line in self.lines:
-            line.add_to_plot(ax_array[1], self.hyper_c)
+            if self.color == 'greyscale':
+                color = 'black'
+            else:
+                color = self.color
+
+            line.add_to_plot(ax_array[1], self.hyper_c, color)
 
         # Draw the pixelized reconstructed image
         ax_array[2].set_title("Reconstructed image")
@@ -92,16 +100,73 @@ class LineApprox:
         plt.show()
 
 
-class LineApproxBruteForce(LineApprox):
-    """Child class of the LineApprox that uses a brute force method to compute the approximation"""
-    def optimize(self, n_random=100):
+class LineApproxRGB:
+    def __init__(self, line_approx_list):
+        if len(line_approx_list) != 3:
+            raise ValueError("Too many channels, cannot interpret as RGB image.")
+
+        self.r_line_approx = line_approx_list[0]
+        self.g_line_approx = line_approx_list[1]
+        self.b_line_approx = line_approx_list[2]
+
+    def show_end_result(self):
+
+        lines = self.r_line_approx.lines + self.g_line_approx.lines + self.b_line_approx.lines
+
+        fig, ax_array = plt.subplots(1, 3, subplot_kw={'aspect': 1}, sharex=True, sharey=True)
+
+        ax_array[0].set_title("Target image")
+        ax_array[0].imshow(self.r_line_approx.target_img, cmap='Greys')
+        ax_array[0].axis([0, self.r_line_approx.width, 0, self.r_line_approx.height])
+        ax_array[0].set_xticks([])
+        ax_array[0].set_yticks([])
+
+        fig.show()
+
+        ax_array[1].set_title("Line approximation image")
+
+        for line_approx in [self.r_line_approx, self.g_line_approx, self.b_line_approx]:
+            for line in line_approx.lines:
+                if line_approx.color == 'greyscale':
+                    color = 'black'
+                else:
+                    color = line_approx.color
+
+                line.add_to_plot(ax_array[1], self.r_line_approx.hyper_c, color)
+
+        ax_array[2].set_title("Reconstructed image")
+        ax_array[2].imshow(self.r_line_approx.recon_img, cmap='Greys')
+        ax_array[2].axis([0, self.r_line_approx.width, 0, self.r_line_approx.height])
+        ax_array[2].set_xticks([])
+        ax_array[2].set_yticks([])
+
+        plt.show()
+
+class Optimizer:
+    def __new__(cls, img):
+        if len(img.shape) == 2:
+            return OptimizerGreyscale(img, color='greyscale')
+
+        elif len(img.shape) == 3:
+            return OptimizerRGB(img)
+
+        else:
+            raise ValueError("Unsupported image format")
+
+
+class OptimizerGreyscale:
+
+    def __init__(self, target_img, color, n_lines=200):
+        self.line_approx = LineApprox(target_img, color)
+
+    def run(self, n_random:int=100):
         """Function used to launch the optimization of the approximated image"""
 
-        for i in range(self.n_lines):
+        for i in range(self.line_approx.n_lines):
 
             # Display the progress
-            if i and not i % 100:
-                print("Iteration ", i, "/", self.n_lines)
+            if i and not (i + 1) % 100:
+                print("Iteration ", (i + 1), "/", self.line_approx.n_lines)
 
             # Initialize an empty batch of line and an empty array for the costs
             batch_lines = []
@@ -109,28 +174,43 @@ class LineApproxBruteForce(LineApprox):
 
             # Create a list of 100 randomly initialized lines
             for k in range(n_random):
-                # t0 = time.time()
-
                 # Initialize a random line
-                line = Line([self.height, self.width])
+                line = Line([self.line_approx.height, self.line_approx.width])
 
                 # Reconstruct the line
-                recon_line = line.reconstruct_line(self.hyper_c)
+                recon_line = line.reconstruct_line(self.line_approx.hyper_c)
 
                 # Compute the cost of the line
-                cost_batch_lines[k] = self.compute_cost(self.recon_img + recon_line)
+                cost_batch_lines[k] = self.line_approx.compute_cost(self.line_approx.recon_img + recon_line)
 
                 # Save the line in the list
                 batch_lines.append(line)
 
             # Find the line with the smallest cost
             best_line = batch_lines[np.argmin(cost_batch_lines)]
-            self.costs.append(np.min(cost_batch_lines))
+            self.line_approx.costs.append(np.min(cost_batch_lines))
 
             # Update reconstruction
-            self.update_recon_img(best_line)
+            self.line_approx.update_recon_img(best_line)
 
-        self.show_end_result()
+        return self.line_approx
+
+
+class OptimizerRGB(OptimizerGreyscale):
+    def __init__(self, img):
+        self.img = img
+        self.line_approx_list = []
+
+    def run(self):
+        colors = ['red', 'green', 'blue']
+
+        for k in range(3):
+            super().__init__(self.img[:, :, k], color=colors[k])
+            self.line_approx_list.append(super().run())
+
+        line_approx_rgb = LineApproxRGB(self.line_approx_list)
+
+        return line_approx_rgb
 
 
 class Line:
